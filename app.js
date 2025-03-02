bba6e11a2998fc7b6891ea67b390a1b7c4d8c85b
kawaiifoot <kawaiifoot649@gmail.com>
2025-03-02 17:03:49 +0700

    changed FAQ to dropdown
    it was gonna be too much stuff to scroll if i just info dumped so i made it dropdown for easier use

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+// JavaScript code for handling FAQ interactions
+const questions = document.querySelectorAll('.faq-question');
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,3 +14,4 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
