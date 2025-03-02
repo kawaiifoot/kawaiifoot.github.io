1648c9f1fe6a03bc1f3c310986e8e81c74f65460
kawaiifoot <kawaiifoot649@gmail.com>
2025-03-02 18:46:13 +0700

    added arrow and animation also info
    added arrow thingy so it shows dropdown also added information regarding jenis and also the sources from journal citations

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-// JavaScript code for handling FAQ interactions
 const questions = document.querySelectorAll('.faq-question');
 questions.forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
+        const arrow = question.querySelector('.arrow');
         answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+        arrow.style.transform = answer.style.display === 'block' ? 'rotate(180deg)' : 'rotate(0deg)';
     });
 });
+
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
@@ -14,4 +16,3 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
-
